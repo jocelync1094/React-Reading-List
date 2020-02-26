4330a04a7da3f5b0e4d91f8e22ac4067e69b1860
Jocelyn Chang <jocelync1094@gmail.com>
2020-02-25 19:02:59 -0800

    made adjustments to server file to deploy to heroku

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
+var MONGODB_URI = process.env.MONGODB_UR || "mongodb://readinglist:a01151994@ds153980.mlab.com:53980/heroku_fs4zkg3c"
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
@@ -16,7 +17,7 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
+mongoose.connect(MONGODB_URI);
 
 // Start the API server
 app.listen(PORT, function() {
